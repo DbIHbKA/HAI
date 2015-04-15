0b85f0778718e37e18b879c2843a9094eb4510af
Danil Petrov <d.petrov@adriver.ru>
2015-04-15 19:51:48 +0300

    Don't finish topsort

--- Search-HAI/src/Algorithms/Graph.hs
+++ Search-HAI/src/Algorithms/Graph.hs
@@ -4,19 +4,31 @@
 ------------------------
 module Algorithms.Graph
     ( Graph
+    -- * Conversion
+    , vertices
     -- * Lists
     , fromList
-    -- * Methods
+    -- * Search Neighbours
     , neighbours
     , neighboursWithEdges
     ) where
 
-import           Data.Map      (Map)
-import qualified Data.Map      as M
-import           Data.Maybe    (fromMaybe)
+import           Data.Map   (Map)
+import qualified Data.Map   as M
+import           Data.Maybe (fromMaybe)
+import qualified Data.Set   as S
 
 -- | A Graph with vertex @v@ and edges @e@
 type Graph v e = Map v [(v, e)]
+
+
+{------------------------
+  Conversion
+-------------------------}
+-- | Return all vertices of the graph
+vertices :: Graph v e -> [v]
+vertices = M.keys
+
 
 {------------------------
   Lists
@@ -33,9 +45,9 @@ fromList = foldr
                   m)
         M.empty
 
-{-----------------------
-  Methods
--------------------------}
+{-------------------------
+  Search Neighbours
+--------------------------}
 -- | Return all neighbours of vertex
 neighbours :: Ord v
            => v -> Graph v e -> [v]
@@ -46,3 +58,27 @@ neighbours v graph = map fst $
 neighboursWithEdges :: Ord v
                     => v -> Graph v e -> [(v, e)]
 neighboursWithEdges v graph = fromMaybe [] (M.lookup v graph)
+
+{----------------------------
+  Sort
+-----------------------------}
+topsort :: Graph v e -> Graph v e
+topsort g = topsort' (vertices g) [] [] g
+
+
+topsort' (white:whites) greys black graph = go
+        whites
+        (white : greys)
+        black
+        white
+    where go ws [] bs c = case neighbours c graph of
+                  [] -> topsort' ws [] bs graph
+                  (c':_) -> go ws [c] bs c'
+          go ws (g:gs) bs c = case neighbours c graph of
+                  [] -> go ws gs (c : bs) g
+                  cs -> case notInGreyC
+                                 cs
+                                 (g : gs) of
+                          Nothing -> go ws gs (c : bs) g
+                          Just c' -> go ws (g : gs) bs c'
+          notInGreyC = undefined
