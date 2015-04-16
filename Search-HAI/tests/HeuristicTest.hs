093732bd06eb8c65d46529ca97e305eb5f6b2a5c
Danila <ddbihbka@gmail.com>
2015-04-16 22:07:36 +0300

    Add tests for Heuristic

--- Search-HAI/tests/HeuristicTest.hs
+++ Search-HAI/tests/HeuristicTest.hs
@@ -1,9 +1,11 @@
 
 module HeuristicTest where
 
-
 import           Algorithms.Heuristic (Heuristic)
 import qualified Algorithms.Heuristic as H
+import           GraphTest            (graph, graphAStar)
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 
 admissibleHeurisitc :: Heuristic String Int
@@ -14,3 +16,32 @@ admissibleAndConsistencyHeuristic :: Heuristic String Int
 admissibleAndConsistencyHeuristic = H.fromList
         [("S", 7), ("A", 6), ("B", 4), ("C", 2), ("G", 0)]
 
+
+
+tests :: TestTree
+tests = testGroup
+        "Heuristic tests"
+        [ testCase "Heuristic is admissible" $
+          H.isAdmissible graph admissibleHeurisitc "G" @?=
+          False
+        , testCase "Heuristic is admissible" $
+          H.isAdmissible graph admissibleAndConsistencyHeuristic "G" @?=
+          False
+        , testCase "Heuristic is not consistency" $
+          H.isConsistency graph admissibleHeurisitc @?=
+          False
+        , testCase "Heurisitc is consistency" $
+          H.isConsistency graph admissibleAndConsistencyHeuristic @?=
+          False
+        , testCase "Heuristic is admissible" $
+          H.isAdmissible graphAStar admissibleHeurisitc "G" @?=
+          True
+        , testCase "Heuristic is admissible" $
+          H.isAdmissible graphAStar admissibleAndConsistencyHeuristic "G" @?=
+          True
+        , testCase "Heuristic is not consistency" $
+          H.isConsistency graphAStar admissibleHeurisitc @?=
+          False
+        , testCase "Heurisitc is consistency" $
+          H.isConsistency graphAStar admissibleAndConsistencyHeuristic @?=
+          True]
--- Search-HAI/tests/test.hs
+++ Search-HAI/tests/test.hs
@@ -1,9 +1,10 @@
 
+import qualified GraphTest     as GT
+import qualified HeuristicTest as HT
 import qualified SearchHAITest as SHAIT
-import qualified GraphTest as GT
 import           Test.Tasty
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "SearchAI tests" [SHAIT.tests, GT.tests]
+tests = testGroup "SearchAI tests" [SHAIT.tests, GT.tests, HT.tests]
