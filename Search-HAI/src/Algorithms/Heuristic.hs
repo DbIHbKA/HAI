d6da54ae9b188db17d029dd77daa3d7cb22ed9c1
Danila <ddbihbka@gmail.com>
2015-04-16 22:07:17 +0300

    Add checks on properties like admissible and consistency

--- Search-HAI/src/Algorithms/Heuristic.hs
+++ Search-HAI/src/Algorithms/Heuristic.hs
@@ -6,10 +6,14 @@ module Algorithms.Heuristic
     ( Heuristic
     -- * Lists
     , fromList
-
+    -- * Properties
+    , isAdmissible
+    , isConsistency
     ) where
 
-import qualified Data.Map as M
+import           Algorithms.Graph (Graph, floydwarshall, vertices)
+import qualified Data.Map         as M
+
 
 -- | A Heuristic with keys @e@ and values @v@
 type Heuristic e v = (e -> v)
@@ -20,3 +24,45 @@ type Heuristic e v = (e -> v)
 -- | Create Heuristic from list
 fromList :: (Ord e,Num v,Ord v) => [(e, v)] -> Heuristic e v
 fromList l k = M.findWithDefault 0 k (M.fromList l)
+
+
+{----------------------------
+  Properties
+-----------------------------}
+-- | Check heuristic on admissible for given graph
+-- heruisitc is admissible if \forall A
+-- h(A) \lt actual cost from A to Goal
+isAdmissible :: (Integral e,Ord v)
+             => Graph v e -> Heuristic v e -> v -> Bool
+isAdmissible g h goal = all (`prop` goal) (filter (/= goal) (vertices g))
+    where cost = floydwarshall g
+          prop va vg = fromIntegral
+                  (h va) <=
+              cost va vg
+
+
+-- | Check heuristic on consistency for given graph
+-- heuristic is consistency if \forall A and C
+-- h(A) - h(C) \lt actual cost from A to C
+isConsistency :: (Integral e,Ord v)
+              => Graph v e -> Heuristic v e -> Bool
+isConsistency g h = checkProperty
+        (vertices g)
+        prop
+    where cost = floydwarshall g
+          prop va vg = fromIntegral
+                  (h va -
+                   h vg) <=
+              cost va vg
+
+
+{-------------------------------
+  Internal functions
+--------------------------------}
+checkProperty :: [v] -> (v -> v -> Bool) -> Bool
+checkProperty verts prop = all
+        (\va ->
+              all
+                  (prop va)
+                  verts)
+        verts
